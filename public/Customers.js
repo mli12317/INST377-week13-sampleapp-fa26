9c0a748055cf6ccd2c295a09c15913035bda766c
Megan Li <mli12317@terpmail.umd.edu>
2026-05-14 21:58:38 -0400

    feat: Adding ability to add customers via UI

--- public/Customers.js
+++ public/Customers.js
@@ -1,3 +1,19 @@
+async function createCustomer() {
+    await fetch(`/customer`, {
+        method: 'POST',
+        body: JSON.stringify({
+            firstName: `${document.getElementById('firstName').value}`,
+            lastName: `${document.getElementById('lastName').value}`,
+            state: `${document.getElementById('state').value}`,
+        }),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then((result) => result.json());
+
+    await loadCustomerData();
+}
+
 async function loadCustomerData() {
     await fetch('/customers')
         .then((result) => result.json())
@@ -40,6 +56,11 @@ async function loadCustomerData() {
 
                 table.appendChild(customerTableRow);
             });
+
+            const preExistingTable = document.getElementById('customerInfo');
+            if (preExistingTable) {
+                preExistingTable.remove();
+            }
 
             document.body.appendChild(table);
         });
